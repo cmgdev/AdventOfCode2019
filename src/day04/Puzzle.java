4d720eb4a00a55fa1b33fde89f7df467eb52a6d4
Colin <colin.g.guerrette@gmail.com>
2019-12-05 21:53:31 -0600

    solve day 4, puzzle 2

--- src/day04/Puzzle.java
+++ src/day04/Puzzle.java
@@ -6,6 +6,7 @@ public class Puzzle {
       verifyNothingBroke();
 
       solve1();
+      solve2();
    }
 
    public static void solve1() {
@@ -16,7 +17,7 @@ public class Puzzle {
       int count = 0;
       for ( int i = low; i < high; i++ ) {
          String s = Integer.toString( i );
-         if ( isSixDigitNumber( s ) && twoAdjacentDigitsSame( s ) && digitsNeverDecrease( s ) ) {
+         if ( isSixDigitNumber( s ) && atLeastTwoAdjacentDigitsSame( s ) && digitsNeverDecrease( s ) ) {
             count++;
          }
       }
@@ -24,11 +25,28 @@ public class Puzzle {
       System.out.println( 2779 == count );
    }
 
+   public static void solve2() {
+      System.out.println( "Solving 2..." );
+      //108457-562041
+      int low = 108457;
+      int high = 562041;
+      int count = 0;
+
+      for ( int i = low; i < high; i++ ) {
+         String s = Integer.toString( i );
+         if ( isSixDigitNumber( s ) && atLeastTwoAdjacentDigitsSame( s ) && digitsNeverDecrease( s ) && twoAdjacentDigitsSame( s ) ) {
+            count++;
+         }
+      }
+      System.out.println( "Count is " + count );
+      System.out.println( 1972 == count );
+   }
+
    public static boolean isSixDigitNumber( String input ) {
       return input.matches( "\\d{6}" );
    }
 
-   public static boolean twoAdjacentDigitsSame( String input ) {
+   public static boolean atLeastTwoAdjacentDigitsSame( String input ) {
       for ( int i = 0; i < input.length() - 1; i++ ) {
          if ( input.charAt( i ) == input.charAt( i + 1 ) ) {
             return true;
@@ -46,19 +64,49 @@ public class Puzzle {
       return true;
    }
 
+   public static boolean twoAdjacentDigitsSame( String input ) {
+
+      for ( int i = 1; i < input.length(); i++ ) {
+         int currentAdjacentCount = 1;
+         char previousChar = input.charAt( i - 1 );
+         char currentChar = input.charAt( i );
+
+         while ( currentChar == previousChar ) {
+            currentAdjacentCount++;
+            i++;
+            if( i == input.length() ){
+               break;
+            }
+            else {
+               previousChar = input.charAt( i - 1 );
+               currentChar = input.charAt( i );
+            }
+         }
+         if ( currentAdjacentCount == 2 ) {
+            return true;
+         }
+      }
+
+      return false;
+   }
+
    public static void verifyNothingBroke() {
       System.out.print( "Is everything still working? " );
       System.out.println(
             isSixDigitNumber( "123456" ) == true
-         && isSixDigitNumber( "000000" ) == true
-         && isSixDigitNumber( "123" ) == false
-         && twoAdjacentDigitsSame( "111111" ) == true
-         && twoAdjacentDigitsSame( "122345" ) == true
-         && twoAdjacentDigitsSame( "111123" ) == true
-         && twoAdjacentDigitsSame( "135679" ) == false
-         && twoAdjacentDigitsSame( "123789" ) == false
-         && digitsNeverDecrease( "111111" ) == true
-         && digitsNeverDecrease( "223450" ) == false
+            && isSixDigitNumber( "000000" ) == true
+            && isSixDigitNumber( "123" ) == false
+            && atLeastTwoAdjacentDigitsSame( "111111" ) == true
+            && atLeastTwoAdjacentDigitsSame( "122345" ) == true
+            && atLeastTwoAdjacentDigitsSame( "111123" ) == true
+            && atLeastTwoAdjacentDigitsSame( "135679" ) == false
+            && atLeastTwoAdjacentDigitsSame( "123789" ) == false
+            && digitsNeverDecrease( "111111" ) == true
+            && digitsNeverDecrease( "223450" ) == false
+            && twoAdjacentDigitsSame( "112233" ) == true
+            && twoAdjacentDigitsSame( "123444" ) == false
+            && twoAdjacentDigitsSame( "111122" ) == true
+            && twoAdjacentDigitsSame( "111111" ) == false
       );
 
    }
