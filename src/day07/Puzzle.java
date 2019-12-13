923517cfeacea9685f956142bad63c39753bdb02
Colin <colin.g.guerrette@gmail.com>
2019-12-13 16:58:16 -0600

    make permutations method generic

--- src/day07/Puzzle.java
+++ src/day07/Puzzle.java
@@ -116,26 +116,26 @@ public class Puzzle extends AbstractPuzzle {
    }
 
    // copied from https://stackoverflow.com/a/35946398
-   public static Set<Integer[]> getPermutationsRecursive( Integer[] num ) {
+   public static <T> Set<T[]> getPermutationsRecursive( T[] num ) {
       if ( num == null ) { return null; }
 
-      Set<Integer[]> perms = new HashSet<>();
+      Set<T[]> perms = new HashSet<>();
 
       //base case
       if ( num.length == 0 ) {
-         perms.add( new Integer[0] );
+         perms.add( Arrays.copyOf( num, 0 ) );
          return perms;
       }
 
       //shave off first int then get sub perms on remaining ints.
       //...then insert the first into each position of each sub perm..recurse
 
-      int first = num[0];
-      Integer[] remainder = Arrays.copyOfRange( num, 1, num.length );
-      Set<Integer[]> subPerms = getPermutationsRecursive( remainder );
-      for ( Integer[] subPerm : subPerms ) {
+      T first = num[0];
+      T[] remainder = Arrays.copyOfRange( num, 1, num.length );
+      Set<T[]> subPerms = getPermutationsRecursive( remainder );
+      for ( T[] subPerm : subPerms ) {
          for ( int i = 0; i <= subPerm.length; ++i ) { // '<='   IMPORTANT !!!
-            Integer[] newPerm = Arrays.copyOf( subPerm, subPerm.length + 1 );
+            T[] newPerm = Arrays.copyOf( subPerm, subPerm.length + 1 );
             for ( int j = newPerm.length - 1; j > i; --j ) { newPerm[j] = newPerm[j - 1]; }
             newPerm[i] = first;
             perms.add( newPerm );
