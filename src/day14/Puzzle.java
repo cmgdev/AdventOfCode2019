b687726d65da7ecce5b2eb43bf302c3e210e3a45
cmgdev <cmgdev>
2019-12-17 21:47:29 -0600

    solved day 14, puzzle 2

--- src/day14/Puzzle.java
+++ src/day14/Puzzle.java
@@ -7,7 +7,7 @@ import java.util.stream.Collectors;
 
 public class Puzzle extends AbstractPuzzle {
 
-    public static final boolean IS_TEST = false;
+    public static final boolean IS_TEST = true;
     public static final int DAY = 14;
 
     public static final String FUEL = "FUEL";
@@ -27,30 +27,24 @@ public class Puzzle extends AbstractPuzzle {
         Puzzle puzzle = new Puzzle();
         List<String> instructions = puzzle.readFile();
 
-        Map<String, Reaction> reactions = new HashMap<>();
-        for (String r : instructions) {
-            Reaction reaction = new Reaction(r);
-            reactions.put(reaction.output.name, reaction);
-        }
-
-//        System.out.println(reactions);
+        Map<String, Reaction> reactions = getReactions(instructions);
 
         Reaction fuelReaction = reactions.get(FUEL);
 
-        Map<String, Integer> onHand = new HashMap<>();
-        int oreNeeded = getOreNeeded(fuelReaction.output.name, fuelReaction.output.quantity, reactions, onHand);
+        Map<String, Long> onHand = new HashMap<>();
+        long oreNeeded = getOreNeeded(fuelReaction.output.name, fuelReaction.output.quantity, reactions, onHand);
 
         long expected = puzzle.getAnswer1();
         System.out.println("Ore Needed: " + oreNeeded);
         System.out.println(expected == oreNeeded);
     }
 
-    public static int getOreNeeded(String substanceName, int substanceQuantity, Map<String, Reaction> reactions, Map<String, Integer> onHand) {
+    public static long getOreNeeded(String substanceName, long substanceQuantity, Map<String, Reaction> reactions, Map<String, Long> onHand) {
         Reaction reaction = reactions.get(substanceName);
-        int alreadyHave = onHand.getOrDefault(substanceName, 0);
-        int need = substanceQuantity - alreadyHave;
-        int factor = (int) Math.ceil(Math.max(need, 0) / (double) reaction.output.quantity);
-        int remaining = (reaction.output.quantity * factor) - need;
+        long alreadyHave = onHand.getOrDefault(substanceName, 0L);
+        long need = substanceQuantity - alreadyHave;
+        long factor = (long) Math.ceil(Math.max(need, 0) / (double) reaction.output.quantity);
+        long remaining = (reaction.output.quantity * factor) - need;
         onHand.put(substanceName, remaining);
 
         int ore = 0;
@@ -69,7 +63,41 @@ public class Puzzle extends AbstractPuzzle {
         System.out.println("Solving 2...");
         Puzzle puzzle = new Puzzle();
         List<String> instructions = puzzle.readFile();
+        Map<String, Reaction> reactions = getReactions(instructions);
+
+        Reaction fuelReaction = reactions.get(FUEL);
+
+        long oreInInventory = 1000000000000L;
+        long fuelProduced = 0;
+        boolean keepRunning = true;
+
+        Map<String, Long> onHand = new HashMap<>();
+
+        while( keepRunning ) {
+            long oreNeeded = getOreNeeded(fuelReaction.output.name, fuelReaction.output.quantity, reactions, onHand);
+
+            if( oreInInventory >= oreNeeded ){
+                oreInInventory -= oreNeeded;
+                fuelProduced++;
+                System.out.println( "Fuel produced: " + fuelProduced + ", ore in inventory: " + oreInInventory );
+            }
+            else{
+                keepRunning = false;
+            }
+        }
         long expected = puzzle.getAnswer2();
+        System.out.println("Fuel Produced: " + fuelProduced);
+        System.out.println(expected == fuelProduced);
+
+    }
+
+    private static Map<String, Reaction> getReactions(List<String> instructions) {
+        Map<String, Reaction> reactions = new HashMap<>();
+        for (String r : instructions) {
+            Reaction reaction = new Reaction(r);
+            reactions.put(reaction.output.name, reaction);
+        }
+        return reactions;
     }
 
     public static class Substance {
