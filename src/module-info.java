2a3f8dadb6c9e2e70890102d92d645ede11af98f
cmgdev <>
2021-09-21 12:06:34 -0500

    files created/modified by eclipse

---
+++ src/module-info.java
@@ -0,0 +1,23 @@
+module AdventOfCode2019 {
+	exports day09;
+	exports day08;
+	exports day07;
+	exports day06;
+	exports day17;
+	exports day05;
+	exports day16;
+	exports day04;
+	exports day15;
+	exports day03;
+	exports day14;
+	exports day02;
+	exports day13;
+	exports day01;
+	exports day12;
+	exports day11;
+	exports day10;
+	exports base.model;
+	exports base;
+
+	requires java.desktop;
+}
